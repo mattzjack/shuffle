f9db6623f724e036ae95b5bb64ed171595f937cc
Vibius Vibidius Zosimus <mudsharkmk@gmail.com>
2016-12-02 22:25:05 -0500

    fun stuff

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $("h1").click(function(){
-        $(this).hide();
+h1oddhidden = false;
+$(function(){
+    alert("aha");
+    $("h1:first").click(function(){
+        if (h1oddhidden) {
+            $("h1:odd").show();
+            h1oddhidden = false;
+        } else if (!h1oddhidden) {
+            $("h1:odd").hide();
+            h1oddhidden = true;
+        } else {
+            console.log("err");
+        }
     });
 });
